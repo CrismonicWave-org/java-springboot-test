f52364bb5e55379bd43dbe897a4aa08c12ca25cb
Kenneth Crismon <kencrismon@crismonicwave.com>
2022-12-01 16:05:32 -0700

    -Adding a known secret

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+	private string userId = "kcrismon";
+	private string password = "KnownPassword1234!";
+	
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
